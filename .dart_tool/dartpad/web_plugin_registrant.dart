765db90c77e1eb4d8d12ba2af1effd7a22b3f838
Kristifor Milchev <km@airzen.io>
2023-06-10 22:32:09 +0300

    fixing project refereces

---
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -0,0 +1,16 @@
+// Flutter web plugin registrant file.
+//
+// Generated file. Do not edit.
+//
+
+// @dart = 2.13
+// ignore_for_file: type=lint
+
+import 'package:flutter_dropzone_web/flutter_dropzone_plugin.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void registerPlugins([final Registrar? pluginRegistrar]) {
+  final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  FlutterDropzonePlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
