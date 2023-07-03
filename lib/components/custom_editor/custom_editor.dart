2b50412e3233a07bd89767916d04ea0e5fcc7a91
Kristifor Milchev <km@airzen.io>
2023-07-03 18:12:23 +0300

    Adding the editor as a submodule

--- lib/components/custom_editor/custom_editor.dart
+++ lib/components/custom_editor/custom_editor.dart
@@ -1,11 +1,15 @@
-import 'package:editor/main.dart';
 import 'package:flutter/material.dart';
+import 'package:presentation/components/custom_editor/custom_editor_viewmodel.dart';
+import 'package:stacked/stacked.dart';
 
 class CustomEditor extends StatelessWidget {
   const CustomEditor({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Editor();
+    return ViewModelBuilder.reactive(
+      viewModelBuilder: () => CustomEditorViewModel(),
+      builder: (context, viewModel, child) => Placeholder(),
+    );
   }
 }
---
+++ lib/components/custom_editor/custom_editor_viewmodel.dart
@@ -0,0 +1,3 @@
+import 'package:presentation/page_view_model.dart';
+
+class CustomEditorViewModel extends PageViewModel {}
